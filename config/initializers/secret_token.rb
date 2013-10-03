1caf4fcd251c30735ca8f1c205ee1945d40ac6e7
長砂 純也 <nagasuna@gmail.com>
2013-10-03 23:18:41 +0900

    rstokyoPJ新規作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rstokyo::Application.config.secret_key_base = '71f70c46811a346bb51be7248540e49f26827812768a6c2f7abbfdddf486ef2374c67618ac608ca2f68aa5196048b2f5a50ecf15ed9d595819a34764c8b9c70f'
