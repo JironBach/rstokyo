51080a9bcf936df662985263b79c5a865facad09
Mobin <nagasuna@gmail.com>
2013-10-30 00:42:17 +0900

    migrateとview修正

--- db/migrate/20131029150403_create_mail_contacts.rb
+++ db/migrate/20131029150403_create_mail_contacts.rb
@@ -1,8 +1,11 @@
 class CreateMailContacts < ActiveRecord::Migration
   def change
-    create_table :mail_contacts do |t|
-
+    create_table(:mail_contacts) do |t|
       t.timestamps
+      t.string(:corp_name, :limit => 50, :null => (false))
+      t.string(:name, :limit => 50, :null => (false))
+      t.string(:email, :limit => 50, :null => (false))
+      t.string(:detail, :default => "", :null => (false))
     end
   end
 end
