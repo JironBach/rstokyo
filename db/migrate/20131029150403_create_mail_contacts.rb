beb80ffff337d7bffb00285e1dc79dc9696102a8
Mobin <nagasuna@gmail.com>
2013-10-31 00:59:43 +0900

    お問い合わせ：名前の最大値を50に変更

--- db/migrate/20131029150403_create_mail_contacts.rb
+++ db/migrate/20131029150403_create_mail_contacts.rb
@@ -3,9 +3,9 @@ class CreateMailContacts < ActiveRecord::Migration
     create_table(:mail_contacts) do |t|
       t.timestamps
       t.string(:corp_name, :limit => 50, :null => (false))
-      t.string(:name, :limit => 100, :null => (false))
+      t.string(:name, :limit => 50, :null => (false))
       t.string(:email, :limit => 100, :null => (false))
-      t.text(:detail, :default => nil, :null => (false))
+      t.text(:detail, :null => (false))
     end
   end
 end
