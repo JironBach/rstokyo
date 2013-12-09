8497ed42d7bc9edefa7615c70649cdebfa329906
mobin <nagasuna@gmail.com>
2013-12-09 23:28:49 +0900

    in_progress #18 [form]こんな物件を探してほしい【Model設定】

--- db/seeds.rb
+++ db/seeds.rb
@@ -103,4 +103,60 @@ MasterOwnerKubun.first_or_create([
 	{ id: 3, value: 'その他' },
 ])
 
+MasterContactInformation.first_or_create([
+	{ id: 1, value: '電話' },
+	{ id: 2, value: 'メール' },
+])
 
+MasterCommutingTime.first_or_create([
+  { id: 1, value: '10分以内' },
+  { id: 2, value: '20分以内' },
+  { id: 3, value: '30分以内' },
+  { id: 4, value: '40分以内' },
+  { id: 5, value: '50分以内' },
+  { id: 6, value: '1時間以内' },
+  { id: 7, value: '1時間半以内' },
+  { id: 8, value: '2時間以内' },
+])
+
+MasterHopeRrent.first_or_create([
+  { id: 1, value: '3万～3.5万円' },
+  { id: 2, value: '3.5万～4万円' },
+  { id: 3, value: '4万～4.5万円' },
+  { id: 4, value: '4.5万～5万円' },
+  { id: 5, value: '5万～5.5万円' },
+  { id: 6, value: '5.5万～6万円' },
+  { id: 7, value: '6万～6.5万円' },
+  { id: 8, value: '6.5万～7万円' },
+  { id: 9, value: '7万～7.5万円' },
+  { id: 10, value: '7.5万～8万円' },
+  { id: 11, value: '8万～8.5万円' },
+  { id: 12, value: '8.5万～9万円' },
+  { id: 13, value: '9万～9.5万円' },
+  { id: 14, value: '9.5万～10万円' },
+  { id: 15, value: '10万～10.5万円' },
+  { id: 16, value: '10.5万～11万円' },
+  { id: 17, value: '11万～11.5万円' },
+  { id: 18, value: '11.5万～12万円' },
+  { id: 19, value: '12万～12.5万円' },
+  { id: 20, value: '12.5万～13万円' },
+  { id: 21, value: '13万～13.5万円' },
+  { id: 22, value: '13.5万～14万円' },
+  { id: 23, value: '14万～14.5万円' },
+  { id: 24, value: '14.5万～15万円' },
+  { id: 25, value: '15万～20万円' },
+  { id: 26, value: '20万～30万円' },
+  { id: 27, value: '30万～40万円' },
+  { id: 28, value: '40万～50万円' },
+  { id: 29, value: '50万～100万円' },
+  { id: 30, value: '100万～' },
+])
+
+MasterDesiredNumber.first_or_create([
+  { id: 1, value: '１人' },
+  { id: 2, value: '２人' },
+  { id: 3, value: '３人' },
+  { id: 4, value: '４人' },
+  { id: 5, value: '５人' },
+  { id: 6, value: '６人以上' },
+])
