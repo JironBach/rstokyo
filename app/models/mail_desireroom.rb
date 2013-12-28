a7bd81f7c99b21d7788d0dd1ab3079eb86d17d6c
JironBach <junshimo2@gmail.com>
2013-12-29 00:15:15 +0900

    性別必須。

--- app/models/mail_desireroom.rb
+++ app/models/mail_desireroom.rb
@@ -2,4 +2,5 @@ class MailDesireroom < ActiveRecord::Base
 	has_many :mail_desireroom_master_madoris
 	has_many :master_madoris, :through => :mail_desireroom_master_madoris
 	accepts_nested_attributes_for :master_madoris
+  validates :master_gender_id, presence: { message: '※性別を選択してください' }
 end
